eb343d46213f34015172f62c14bcb52724285f21
LeonHo <leon.ho@bchydro.com>
2015-09-24 14:32:59 -0700

    Fixed first controller link & added htaccess

---
+++ application/controllers/First.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Our homepage. Show a table of all the author pictures. Clicking on one should show their quote.
+ * Our quotes model has been autoloaded, because we use it everywhere.
+ * 
+ * controllers/First.php
+ *
+ * ------------------------------------------------------------------------
+ */
+class First extends Application {
+
+    function __construct() {
+        parent::__construct();
+    }
+
+    //-------------------------------------------------------------
+    //  The normal pages
+    //-------------------------------------------------------------
+
+    function index() {
+        $this->data['pagebody'] = 'justone';    // this is the view we want shown
+        // build the list of authors, to pass on to our view
+        $source = $this->quotes->first();
+        $this->data['what'] = $source['what'];
+        $this->data['who'] = $source['who'];
+        $this->data['mug'] = $source['mug'];
+        $this->render();
+    }
+
+}
