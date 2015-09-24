006ebb70f49f19beb4db1f6ddcf3cec9f94d2f83
LeonHo <leon.ho@bchydro.com>
2015-09-24 15:11:51 -0700

    Added route for last, and sleep

--- application/controllers/last/Welcome.php
+++ application/controllers/last/Welcome.php
@@ -4,11 +4,11 @@
  * Our homepage. Show a table of all the author pictures. Clicking on one should show their quote.
  * Our quotes model has been autoloaded, because we use it everywhere.
  * 
- * controllers/First.php
+ * controllers/controllers/last/Last.php
  *
  * ------------------------------------------------------------------------
  */
-class First extends Application {
+class Welcome extends Application {
 
     function __construct() {
         parent::__construct();
@@ -21,20 +21,10 @@ class First extends Application {
     function index() {
         $this->data['pagebody'] = 'justone';    // this is the view we want shown
         // build the list of authors, to pass on to our view
-        $source = $this->quotes->first();
+        $source = $this->quotes->last();
         $this->data['what'] = $source['what'];
         $this->data['who'] = $source['who'];
         $this->data['mug'] = $source['mug'];
-        $this->render();
-    }
-    function zzz() {
-        $this->data['pagebody'] = 'justone';    // this is the view we want shown
-        // build the list of authors, to pass on to our view
-        $source = $this->quotes->get(1);
-        $this->data['what'] = $source['what'];
-        $this->data['who'] = $source['who'];
-        $this->data['mug'] = $source['mug'];
-
         $this->render();
     }
 
